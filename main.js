d11ba21e885faf9bcac8e00066c5d42e546848d1
SamrudhNelli <samrudhnelli@gmail.com>
2025-07-28 14:39:06 +0530

    Fix all bugs

--- main.js
+++ main.js
@@ -1,61 +1,73 @@
 var data = JSON.stringify({
     "clientcode":"S55125542",
-    "password":"Trading@Project123",
-	"totp":"5JENCOQR3472DBTJCGJA4LK7RE",
-  "state":"STATE_VARIABLE"
+    "password":"8289",
+	  "totp":"461715",
+    "source":"WEB",
+    "state":"STATE_VARIABLE"
 });
+console.log("Meow!");
 
 var config = {
   method: 'post',
   url: 'https://apiconnect.angelone.in/rest/auth/angelbroking/user/v1/loginByPassword',
   headers : {
+    'X-PrivateKey': 'VY6lImzK',
     'Content-Type': 'application/json',
     'Accept': 'application/json',
     'X-UserType': 'USER',
     'X-SourceID': 'WEB',
-    'X-ClientLocalIP': '127.0.0.1', 
+    'X-ClientLocalIP': '127.0.0.1',
     'X-ClientPublicIP': '203.110.242.30',
-    'X-MACAddress': '7c:b5:66:fa:08:d9',
-    'X-PrivateKey': 'VY6lImzK'
+    'X-MACAddress': '7c:b5:66:fa:08:d9'
   },
   data : data
 };
 
+var jwtToken;
+
 axios(config)
-.then(function (response) {
-  console.log(JSON.stringify(response.data));
+.then(function (response) 
+{
+  jwtToken = response.data.data.jwtToken;
+  console.log("Login successful!");
+  console.log("JWT Token: " + jwtToken);
+  setInterval(function() 
+  {
+      var data = JSON.stringify({
+        "mode": "FULL", "exchangeTokens": {
+            "NSE": ["3045"] }});
+      var config = {
+      method: 'post',
+      url: 'https://apiconnect.angelone.in/rest/secure/angelbroking/market/v1/quote/',
+      headers: { 
+        'X-PrivateKey': 'VY6lImzK', 
+        'Accept': 'application/json', 
+        'X-SourceID': 'WEB', 
+        'X-ClientLocalIP': '127.0.0.1', 
+        'X-ClientPublicIP': '203.110.242.30', 
+        'X-MACAddress': '7c:b5:66:fa:08:d9', 
+        'X-UserType': 'USER', 
+        'Authorization': `Bearer ${jwtToken}`, 
+        'Content-Type': 'application/json'
+      },
+      data : data
+      };
+
+      console.log('HOla!');
+
+      axios(config)
+      .then(function (response) {
+        console.log(JSON.stringify(response.data));
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, 5000);
+
 })
+
 .catch(function (error) {
   console.log(error);
 });
 
 
-var data1 = JSON.stringify({
-     "mode": "LTP", "exchangeTokens": { "NSE": ["3045","881"], "NFO": ["58662"] }}
-);
-var config1 = {
-  method: 'post',
-  url: 'https://apiconnect.angelone.in/rest/secure/angelbroking/market/v1/quote/',
-  headers: { 
-    'X-PrivateKey': 'VY6lImzK', 
-    'Accept': 'application/json, application/json', 
-    'X-SourceID': 'WEB, WEB', 
-    'X-ClientLocalIP': '127.0.0.1', 
-    'X-ClientPublicIP': '203.110.242.30', 
-    'X-MACAddress': '7c:b5:66:fa:08:d9', 
-    'X-UserType': 'USER', 
-    'Authorization': 'c9c657c0-467b-4d3c-8e6e-359c6602abd3', 
-    'Content-Type': 'application/json'
-  },
-  data : data1
-};
-
-console.log('HOla!');
-
-axios(config1)
-.then(function (response) {
-  console.log(JSON.stringify(response.data));
-})
-.catch(function (error) {
-  console.log(error);
-});
